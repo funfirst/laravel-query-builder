63aae4fd799ac4fda5b817ade29222e1adc2ee02
Jakub Gause <gause.jakub@gmail.com>
2019-09-13 10:53:19 +0200

    Pagination parameters renamed

--- src/Concerns/AppliesPagination.php
+++ src/Concerns/AppliesPagination.php
@@ -6,8 +6,22 @@ trait AppliesPagination
 {
     public function applyPagination()
     {
-        $perPage = $this->request->perPage ?: 25; //FIXME: TEMP FOR TESTING
-        $currentPage = $this->request->currentPage ?: 1; //FIXME: TEMP FOR TESTING
+        if ($this->request->page) {
+            if (array_key_exists('number', $this->request->page)) {
+                $currentPage = $this->request->page['number'];
+            } else {
+                $currentPage = 1;
+            }
+
+            if (array_key_exists('size', $this->request->page)) {
+                $perPage = $this->request->page['size'];
+            } else {
+                $perPage = 1;
+            }
+        } else {
+            $perPage = 25;
+            $currentPage = 1;
+        }
 
         $this->offset($perPage * $currentPage - $perPage)->take($perPage);
 
