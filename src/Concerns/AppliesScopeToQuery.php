41d704d7f562a6602838063448764e3972a2d441
Jakub Gause <gause.jakub@gmail.com>
2019-09-30 09:48:09 +0200

    applyScopes method now can use default scopes from method parameters

--- src/Concerns/AppliesScopeToQuery.php
+++ src/Concerns/AppliesScopeToQuery.php
@@ -4,12 +4,13 @@ namespace Spatie\QueryBuilder\Concerns;
 
 trait AppliesScopeToQuery
 {
-    public function applyScopes()
+    public function applyScopes($defaultScopes = [])
     {
         $scopes = $this->request->scopes; //FIXME: TEMP FOR TESTING
         if ($scopes === null) {
             $scopes = [];
-        } 
+        }
+        $scopes = array_merge($scopes, $defaultScopes);
         foreach ($scopes as $scope) {
             $model = $this->getModel();
             if (method_exists($model, 'scope' . ucfirst($scope))) {
