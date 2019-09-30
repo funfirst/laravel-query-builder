06859c2326ca5061f8061ee7c34374b5bfa66c48
Jakub Gause <gause.jakub@gmail.com>
2019-09-30 10:06:32 +0200

    Search temp updated

--- src/Concerns/SearchInQuery.php
+++ src/Concerns/SearchInQuery.php
@@ -6,27 +6,50 @@ trait SearchInQuery
 {
     public function search()
     {
-        $search = $this->request->search; //FIXME: TEMP FOR TESTING
+        // $search = $this->request->search; //FIXME: TEMP FOR TESTING
+        // if ($search !== null) {
+        //     $searchParts = explode(' ', $search);
+        //     $this->where(function ($q) use ($searchParts) {
+        //         foreach($searchParts as $searchPart) {
+        //             $model = $this->getModel();
+        //             if (!$model->customQuerySearch($q)) {
+        //                 $modelSearchables = $model->getSearchableFields();
+        //                 $q->where(function ($query) use ($modelSearchables, $searchPart) {
+        //                     $query->where(function ($q) use ($modelSearchables, $searchPart) {
+        //                         foreach ($modelSearchables as $modelSearchable) {
+        //                             $q->orWhere($modelSearchable, 'LIKE', '%' . $searchPart . '%');
+        //                         }
+        //                     });
+        //                 });
+        //             }
+        //         }
+        //     });
+        // }
+        // return $this;
+
+        $search = $this->request->search;
         if ($search !== null) {
             $searchParts = explode(' ', $search);
             $this->where(function ($q) use ($searchParts) {
-                foreach($searchParts as $searchPart) {
+                foreach ($searchParts as $searchPart) {
                     $model = $this->getModel();
                     $modelSearchables = [];
-
                     $q->where(function ($query) use ($modelSearchables, $searchPart) {
                         $query->where(function ($q) use ($modelSearchables, $searchPart) {
                             foreach ($modelSearchables as $modelSearchable) {
                                 $q->orWhere($modelSearchable, 'LIKE', '%' . $searchPart . '%');
                             }
-                        })->orWhereHas('properties', function($q) use ($searchPart) {
-                            $q->where(function($query) use ($searchPart) {
-                                $query->where('type', 'SYSTEM')
-                                    ->where('value', 'LIKE', '%' . $searchPart . '%');
-                            });
                         });
+
+                        $usesProperties = array_key_exists(\App\API\Properties\Traits\UsesProperties::class, class_uses($this->getModel()));
+                        if ($usesProperties) {
+                            $query->orWhereHas('properties', function ($q) use ($searchPart) {
+                                $q->where(function ($query) use ($searchPart) {
+                                    $query->where('value', 'LIKE', '%' . $searchPart . '%');
+                                });
+                            });
+                        }
                     });
-                   
                 }
             });
         }
